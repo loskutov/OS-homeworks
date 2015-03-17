3d5b0610d842aadf8624af683095fc6f8aa6d58c
Ignat Loskutov <ignat.loskutov@gmail.com>
2015-03-18 00:03:53 +0300

    HW #2 completed

--- lib/helpers.c
+++ lib/helpers.c
@@ -25,3 +25,18 @@ ssize_t write_(int fd, const void *buf, size_t count) {
     } while (size > 0);
     return offset;
 }
+
+ssize_t read_until(int fd, void *buf, size_t count, char delimiter) {
+    ssize_t size;
+    ssize_t offset;
+    for(offset = 0; offset < count; ++offset) {
+        size = read(fd, buf + offset, 1);
+        if (size == -1)
+            return -1;
+        if (size == 0)
+            return offset;
+        if (((char*)buf)[offset] == delimiter)
+            break;
+    }
+    return offset + 1;
+}
---
+++ revwords/revwords.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <helpers.h>
+
+void swap(char* a, char* b) {
+    *a ^= *b;
+    *b ^= *a;
+    *a ^= *b;
+}
+
+char* reverse(char* buf, size_t size) {
+    for (size_t i = 0; i < size/2; ++i) {
+        swap(buf + i, buf + size - i - 1);
+    }
+    return buf;
+}
+
+int main() {
+    char buf[8192];
+    ssize_t n;
+    ssize_t offset = 0;
+    while ((n = read_until(STDIN_FILENO, buf, sizeof(buf), ' ')) != 0) {
+        if (n == -1) {
+            perror("Error while reading");
+            return EXIT_FAILURE;
+        } else {
+            if (n == 0)
+                return 0;
+            int no_trail = n - (buf[n - 1] == ' ');
+            if (write_(STDOUT_FILENO, reverse(buf, no_trail), n) == -1) {
+                perror("Error while writing");
+                return EXIT_FAILURE;
+            }
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
